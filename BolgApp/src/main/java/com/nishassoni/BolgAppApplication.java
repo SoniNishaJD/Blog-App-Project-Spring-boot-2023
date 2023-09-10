8a58862d5d30582797efa938fa7ee83fdb2ca334
Soni Nisha <soni.nishaa24@gmail.com>
2023-09-10 22:56:35 +0530

    10 SEPT 2023

---
+++ BolgApp/src/main/java/com/nishassoni/BolgAppApplication.java
@@ -0,0 +1,13 @@
+package com.nishassoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolgAppApplication.class, args);
+	}
+
+}
